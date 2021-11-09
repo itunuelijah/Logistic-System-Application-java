485ba228f6e660065081dc78a2cfaef663c58412
itunuelijah <itunuelijah@gmail.com>
2021-11-09 13:09:08 +0100

    logistic system models

---
+++ src/main/java/africa/semicolon/logisticSystem/data/models/Package.java
@@ -0,0 +1,14 @@
+package africa.semicolon.logisticSystem.data.models;
+
+import lombok.Data;
+
+@Data
+public class Package {
+    private Integer id;
+    private String name;
+    private String senderEmail;
+    private String receiverName;
+    private String deliveryAddress;
+    private double netWeight;
+    private String receiverPhone;
+}
---
+++ src/main/java/africa/semicolon/logisticSystem/data/models/Sender.java
@@ -0,0 +1,10 @@
+package africa.semicolon.logisticSystem.data.models;
+
+import lombok.Data;
+
+@Data
+public class Sender {
+    private String emailAddress;
+    private String senderName;
+    private String phoneNumber;
+}
